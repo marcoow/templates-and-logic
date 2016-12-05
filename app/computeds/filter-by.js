a90d47f4916ce5a94f34bdd0bfc5141c9cd96252
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2016-12-05 17:26:41 +0100

    much simplified

--- app/computeds/filter-by.js
+++ app/computeds/filter-by.js
@@ -1,44 +1,47 @@
 import Ember from 'ember';
 
-const { computed, A, observer, addObserver, run: { once } } = Ember;
+const { computed, computed: { filter }, A, observer, defineProperty } = Ember;
 
-const Computer = Ember.Object.extend({
-  init() {
-    this._super(...arguments);
-  },
-
-  _observeElements() {
-    let filterProperty = this.get('filterProperty');
-    this.get('collection').map((element) => {
-      return element.addObserver(filterProperty, () => {
-        once(() => {
-          this.set('computedValue', this.compute());
-          this.get('context').notifyPropertyChange(this.get('key'));
-        });
-      });
-    });
-  },
-
-  dependenciesDidChange: observer('collection.[]', 'filterProperty', function() {
-    this.set('computedValue', this.compute());
+// This implements a filterBy Computed Property macro where the property that's
+// being filtered by is dynamic (only know at runtime).
+const DynamicFilterByComputed = Ember.Object.extend({
+  // This could be implemented in a base class, e.g. Ember.Computed.
+  recompute() {
+    // not super happy with this - there might be a better way…
     this.get('context').notifyPropertyChange(this.get('key'));
-    this._observeElements();
+  },
+
+  contentDidChange: observer('content', function() {
+    this.recompute();
   }),
 
-  compute() {
-    return A(this.get('collection')).filterBy(this.get('filterProperty'));
+  filterPropertyDidChange: observer('filterProperty', function() {
+    let filterProperty = this.get('filterProperty');
+    let property = filter(`collection.@each.${filterProperty}`, (item) => item.get(filterProperty));
+    defineProperty(this, 'content', property);
+  }),
+
+  compute(collection, filterProperty) {
+    this.set('collection', collection);
+    this.set('filterProperty', filterProperty);
+
+    return this.get('content');
   }
 });
 
+// The internals of this could be wrapped in a macro similar Ember.Helper.helper so
+// that this could be shortened to sth. like this:
+//
+// export default function filterBy(collectionName, filterPropertyName) {
+//   return Ember.computed.computize(DynamicFilterByComputed);
+// }
 export default function filterBy(collectionName, filterPropertyName) {
-  let computer = Computer.create({
-    collection: computed.oneWay(`context.${collectionName}`),
-    filterProperty: computed.oneWay(`context.${filterPropertyName}`)
-  });
-  return computed(collectionName, filterPropertyName, function(key) {
-    computer.setProperties({ context: this, key});
-    computer._observeElements();
+  let computer = DynamicFilterByComputed.create();
 
-    return computer.compute();
+  return computed(collectionName, filterPropertyName, function(key) {
+    // not super happy with this - there might be a better way…
+    computer.setProperties({ context: this, key});
+
+    return computer.compute(this.get(collectionName), this.get(filterPropertyName));
   });
 }
